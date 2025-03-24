018f06903eb1c676c13c8ca45e01d91ac5d7b155
MBaroky <m.baroky@gmail.com>
2025-03-24 11:25:43 +0200

    add ProfileButton component and implement server-side session check in Profiles page

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -1,8 +1,27 @@
+
+import { ProfileButton } from '@/components/ProfileButton'
+import { authOptions } from '@/lib/authOptions'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-function Profiles() {
+async function Profiles() {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect('/auth')
+  }
   return (
-    <div className='text-white'>Profiles</div>
+    <div className='flex items-center h-full justify-center'>
+      <div className="flex flex-col">
+        <div className="text-3xl md:text-6xl text-center text-white">
+          <h1>Who is watching?</h1>
+        </div>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <ProfileButton />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/ProfileButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { redirect } from "next/navigation";
+
+export const ProfileButton = () => {
+    const {data:user} = useCurrentUser();
+  return (
+    <div onClick={() => {redirect('/')}}>
+      <div className='group flex-row w-44 mx-auto'>
+        <div
+          className='w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent
+              group-hover:border-white
+              group-hover:cursor-pointer
+              overflow-hidden
+              '>
+          <img src='/images/profile-blue.jpg' alt='' />
+        </div>
+        <div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white'>
+            {user?.name}
+        </div>
+      </div>
+    </div>
+  );
+};
